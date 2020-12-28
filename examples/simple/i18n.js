18147dc8668c7c1096b530dfe33b116391a466bb
s3214 <s3214@yahoo.com>
2020-12-27 23:25:16 -0500

    moved footer.json to , trying to demonstrate addPath

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -5,5 +5,36 @@ const path = require('path')
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
   localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localePath: path.resolve('./public/static/locales'),
+
+  //   derivative of @artze solution, updated to match the simple project locales folder structure.
+  backend: {
+    loadPath: function(lng, ns) {
+      if (typeof window == "undefined") {
+        // return path.resolve(`./public/locales/${lng}/${ns}.json`);
+        return path.resolve(`./public/static/locales/${lng}/${ns}.json`);
+      } else {
+        return `/locales/${lng}/${ns}.json`;
+      }
+    },
+
+    // ----
+    // These "addPath" options don't actually add the path...
+    // Trying to figure out how to make the paths available.
+    // ----
+    // addPath: "/locales/add/{{lng}}/{{ns}}.json"
+    // addPath: "http://localhost:3000/static/locales/add/{{lng}}/{{ns}}.json"  // Note, these locations are available via web browser
+    // addPage: function(lng, ns) {
+    //   return `http://localhost:3000/static/locales/add/${lng}/${ns}.json`
+    // },
+    addPage: function(lng, ns) {
+      if (typeof window == "undefined") {
+        // return path.resolve(`./public/locales/${lng}/${ns}.json`);
+        return path.resolve(`./public/static/locales/add/${lng}/${ns}.json`);
+      } else {
+        return `http://localhost:3000/static/locales/add/${lng}/${ns}.json`;
+      }
+    },
+  }
+
 })
